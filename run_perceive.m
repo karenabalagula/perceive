91ecd32450a585122c3826cd4e3e135c74bc8cc2
karenabalagula <karena.balagula@ucsf.edu>
2024-10-22 11:38:46 -0700

    run script

--- run_perceive.m
+++ run_perceive.m
@@ -2,5 +2,5 @@ clc; clear; close all
 
 
 %%
-perceive('Report_Json_Session_Report_20240725T123444.json', 'JSON_ID_26', ...
+perceive('Report_Json_Session_Report_20240828T141005 (2).json', 'JSON_ID_30', ...
     '', true)
